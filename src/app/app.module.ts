7877f9860e68898c866f9fef0e322cf45addc644
Chinwe Ren <chinwe.ren@sap.com>
2019-02-18 15:15:18 +0800

    try guard:canDeactivate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,7 +41,21 @@ import { EventRouteActivator } from './events/event-details/event-router-activat
       }
     )
   ],
-  providers: [EventService, EventRouteActivator],
+  providers: [
+    EventService, 
+    EventRouteActivator,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    }
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if(component.isDirty) {
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  }
+  return true
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,7 +6,8 @@ import { Error404Component } from './errors/404.component';
 import { EventRouteActivator } from './events/event-details/event-router-activator.service';
 
 export const appRoutes: Routes = [
-    { path: 'events/new', component: CreateEventComponent },
+    { path: 'events/new', component: CreateEventComponent,
+        canDeactivate: ['canDeactivateCreateEvent'] },
     { path: 'events', component: EventsListComponent },
     { path: 'events/:id', component: EventDetailsComponent,
         canActivate: [EventRouteActivator] },
