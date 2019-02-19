dbd1b495ec66e6bb90a8189d89942600f1582570
Chinwe Ren <chinwe.ren@sap.com>
2019-02-19 16:39:06 +0800

    add function: add and cancel session

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ISession, restrictedWords } from '../shared/index'
 
 @Component({
+    selector: 'create-session',
     templateUrl: './create-session.component.html',
     styles: [`
     em { float:right; color: #E05C64; padding-left: 10px; }
@@ -13,6 +14,8 @@ import { ISession, restrictedWords } from '../shared/index'
     `]
 })
 export class CreateSessionComponent implements OnInit {
+    @Output() saveNewSession = new EventEmitter()
+    @Output() cancelAddSession = new EventEmitter()
     newSessionForm: FormGroup
     name: FormControl
     presenter: FormControl
@@ -47,6 +50,10 @@ export class CreateSessionComponent implements OnInit {
             abstract: formValues.abstract,
             voters: []
         }
-        console.log(session)
+        this.saveNewSession.emit(session)
+    }
+
+    cancel() {
+        this.cancelAddSession.emit()
     }
 }
--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { EventService } from '../shared/event.service'
 import { ActivatedRoute } from '@angular/router';
 import { IEvent } from '../shared/index';
+import { ISession } from '../shared/index';
 
 @Component({
     templateUrl: './event-details.component.html',
@@ -13,16 +14,34 @@ import { IEvent } from '../shared/index';
     .event-image {
         height: 100px;
     }
+    a { cursor:pointer }
     `]
 })
 export class EventDetailsComponent implements OnInit {
     event:IEvent
+    addMode: boolean
     constructor(private eventService: EventService,
         private route:ActivatedRoute) {
     }
 
     ngOnInit(){
         this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
+    }
+
+    addSession() {
+        this.addMode = true
+    }
+
+    saveNewSession(session:ISession) {
+        const nextId = Math.max.apply(null, this.event.sessions.map(s=>s.id));
+        session.id = nextId + 1
+        this.event.sessions.push(session)
+        this.eventService.updateEvent(this.event)
+        this.addMode = false
+    }
+
+    cancelAddSession() {
+        this.addMode = false
     }
 }
 
