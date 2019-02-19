8f9792840c63d2efb7c8ee5473347e777cb69228
Chinwe Ren <chinwe.ren@sap.com>
2019-02-19 16:10:40 +0800

    create session list to show sessions of one event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import {
   CreateEventComponent,
   EventRouteActivator,
   EventListResolver,
-  CreateSessionComponent
+  CreateSessionComponent,
+  SessionListComponent
 } from './events/index';
 
 import { EventsAppComponent } from './events-app.component';
@@ -33,7 +34,8 @@ import { AuthService } from './user/auth.service';
     NavBarComponent,
     CreateEventComponent,
     Error404Component,
-    CreateSessionComponent
+    CreateSessionComponent,
+    SessionListComponent
   ],
   imports: [
     CommonModule,
