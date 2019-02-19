f1ddd03e8c28c94854b4e04cffdebf8022d12482
Chinwe Ren <chinwe.ren@sap.com>
2019-02-19 15:12:11 +0800

    create customize validator

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ISession } from '../shared/index'
 
@@ -26,8 +26,8 @@ export class CreateSessionComponent implements OnInit {
         this.duration = new FormControl('', Validators.required)
         this.level = new FormControl('', Validators.required)
         this.abstract = new FormControl('', [Validators.required,
-            Validators.maxLength(400)])
-        
+        Validators.maxLength(400), this.restrictedWords(['foo', 'bar'])])
+
         this.newSessionForm = new FormGroup({
             name: this.name,
             presenter: this.presenter,
@@ -37,8 +37,21 @@ export class CreateSessionComponent implements OnInit {
         })
     }
 
+    private restrictedWords(words) {
+        return (control: FormControl): { [key: string]: any } => {
+            if(!words) return null
+            var invalidWords = words
+                .map(w => control.value.includes(w) ? w : null)
+                .filter(w => w != null)
+            
+            return invalidWords && invalidWords.length > 0
+                ? {'restrictedWords': invalidWords.join(', ')}
+                : null
+        }
+    }
+
     saveSession(formValues) {
-        let session:ISession = {
+        let session: ISession = {
             id: undefined,
             name: formValues.name,
             duration: +formValues.duration,
