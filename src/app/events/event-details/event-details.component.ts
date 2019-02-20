54920a516868a6d75311e8e9ba5dec07404672c7
Chinwe Ren <chinwe.ren@sap.com>
2019-02-20 13:48:57 +0800

    fix bug that can't navigate from the pop up dialog

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from '../shared/event.service'
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { IEvent } from '../shared/index';
 import { ISession } from '../shared/index';
 
@@ -28,7 +28,10 @@ export class EventDetailsComponent implements OnInit {
     }
 
     ngOnInit(){
-        this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
+        this.route.params.forEach((params: Params) => {
+            this.event = this.eventService.getEvent(+params['id']);
+            this.addMode = false;
+        })
     }
 
     addSession() {
