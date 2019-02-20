1ff504ba1f59ffa5d83b46db8af2bd534eb3ff1b
Chinwe Ren <chinwe.ren@sap.com>
2019-02-20 12:32:07 +0800

    add search session function

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core'
+import { Injectable, EventEmitter } from '@angular/core'
 import { Subject, Observable } from 'rxjs'
 import { IEvent } from './event.model'
-
+import { ISession } from './event.model'
 
 @Injectable()
 export class EventService {
@@ -29,6 +29,26 @@ export class EventService {
       EVENTS[index] = event
     }
 
+    searchSessions(searchTerm: string) {
+      var term = searchTerm.toLocaleLowerCase();
+      var results: ISession[] = [];
+
+      EVENTS.forEach(event => {
+        var matchingSessions = event.sessions.filter(session => 
+          session.name.toLocaleLowerCase().indexOf(searchTerm) > -1);
+        matchingSessions = matchingSessions.map((session:any) => {
+          session.eventId = event.id;
+          return session;
+        })
+        results = results.concat(matchingSessions);
+      })
+
+      var emitter = new EventEmitter(true);
+      setTimeout(() => {
+        emitter.emit(results);
+      }, 100);
+      return emitter;
+    }
 }
 
 const EVENTS:IEvent[] = [
--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core'
 import { AuthService } from '../user/auth.service'
+import { ISession } from '../events/shared/event.model'
+import { EventService } from '../events/shared/event.service';
 
 @Component({
     selector: 'nav-bar',
@@ -12,5 +14,17 @@ import { AuthService } from '../user/auth.service'
     `]
 })
 export class NavBarComponent {
-    constructor(private auth: AuthService){ }
+    searchTerm: string = "";
+    foundSessions: ISession[];
+
+    constructor(private auth: AuthService, private eventService: EventService){ }
+
+    searchSessions(searchTerm) {
+        this.eventService.searchSessions(searchTerm)
+            .subscribe(sessions => {
+                this.foundSessions = sessions;
+                console.log('this.foundSessions:')
+                console.log(this.foundSessions)
+            });
+    }
 }
